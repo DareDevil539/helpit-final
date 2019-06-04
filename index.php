cc6f502c76be51f737db37655bd24c2373f732d0
DareDevil539 <olenishak@gmail.com>
2019-06-04 23:32:19 +0300

    added materialize-css

--- index.php
+++ index.php
@@ -6,28 +6,13 @@ class Router {
   private $controller = null;
 
   public function doRoute ($route) {
-    if ($route === "") {
-      $this->controller = new HomeController();
-      $this->controller->action();
-    } else {
-      echo $routeController = $this->getRouteController($route);
-//      $this->controller = new $routeController();
-//      if (!$this->controller) {
-//        die("Error 404. Page is not found!");
-//      } else {
-//        $this->controller->action();
-//      }
-    }
-  }
-
-  private function getRouteController ($route) {
-    $routeArr = explode("-", $route);
-
-    for ($i = 0; $i < count($routeArr); $i++) {
-      $routeArr[$i] = ucfirst($routeArr[$i]);
+    switch ($route) {
+      case "":
+        $this->controller = new HomeController();
+        break;
     }
 
-    return join($routeArr)."Controller";
+    $this->controller->action();
   }
 }
 
